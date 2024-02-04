f58ec2c522c2cd35dbe42e37b07b0c8a4b778716
ErickCif <erickcifwork@gmail.com>
2024-02-04 03:39:37 -0500

    Session pages, starting the about page and fixing the App routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import TalkToUsPage from "./pages/TalkToUsPage";
 import TalkToUsResultPage from "./pages/TalkToUsResultPage";
 import SessionPage from "./pages/SessionPage";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/session" exact element={<SessionPage />} />
           <Route path="/talk-to-us" exact element={<TalkToUsPage />} />
           <Route path="/talk-to-us-result" exact element={<TalkToUsResultPage />} />
+          <Route path="/about" exact element={<AboutPage/>}/>
         </Routes>
       </Router>  
     </div>
