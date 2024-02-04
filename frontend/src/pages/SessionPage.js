e3fa221440101f3b89738c80b04df206b0619d01
ErickCif <erickcifwork@gmail.com>
2024-02-04 00:41:33 -0500

    Session Page is now pretty and works with dummy data

--- frontend/src/pages/SessionPage.js
+++ frontend/src/pages/SessionPage.js
@@ -6,135 +6,135 @@ import sessionpagebackground from "../assets/sessionpagebackground.svg";
 function SessionPage() {
     // TODO: Replace dummy data
     const dummyData = [
-            {
-                "artist": "Nicky Jam",
-                "genres": [
-                    "latin hip hop",
-                    "reggaeton",
-                    "trap latino",
-                    "urbano latino"
-                ],
-                "title": "X (feat. Maluma & Ozuna) - Remix",
-                "image": "https://i.scdn.co/image/ab67616d0000b27343ec4e5bf12be32fc0beaac2",
-                "uri": "spotify:track:6JjPBQfI2Y8nIjnm65X6Pw",
-                "weight": 5.158460415146735
-            },
-            {
-                "artist": "Ozuna",
-                "genres": [
-                    "puerto rican pop",
-                    "reggaeton",
-                    "trap latino",
-                    "urbano latino"
-                ],
-                "title": "Bebé",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:0ulsRBiciReng91DhfVT9D",
-                "weight": 4.720726046469784
-            },
-            {
-                "artist": "Jay Chou",
-                "genres": [
-                    "c-pop",
-                    "mandopop",
-                    "taiwan pop",
-                    "zhongguo feng"
-                ],
-                "title": "蒲公英的約定",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:3JovAXFjc98TkksMfeyIMh",
-                "weight": 2.884160497896886
-            },
-            {
-                "artist": "Prince Royce",
-                "genres": [
-                    "bachata",
-                    "latin hip hop",
-                    "latin pop",
-                    "urbano latino"
-                ],
-                "title": "Cosas de la Peda (feat. Gabito Ballesteros)",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:1RzNRKntEk0KiQE4NFBKmc",
-                "weight": 5.28964254738048
-            },
-            {
-                "artist": "Romeo Santos",
-                "genres": [
-                    "bachata",
-                    "latin hip hop",
-                    "latin pop",
-                    "urbano latino"
-                ],
-                "title": "Mi Santa (feat. Tomatito)",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:4EbAftNM732UGLF8gmIIsX",
-                "weight": 5.28964254738048
-            },
-            {
-                "artist": "CNCO",
-                "genres": [
-                    "boy band",
-                    "latin pop",
-                    "reggaeton",
-                    "urbano latino"
-                ],
-                "title": "Reggaetón Lento (Bailemos)",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:3AEZUABDXNtecAOSC1qTfo",
-                "weight": 4.556474012983767
-            },
-            {
-                "artist": "Ozuna",
-                "genres": [
-                    "puerto rican pop",
-                    "reggaeton",
-                    "trap latino",
-                    "urbano latino"
-                ],
-                "title": "Egoísta",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:4X3CV9rXo3hQrkb0fzRAux",
-                "weight": 4.720726046469784
-            },
-            {
-                "artist": "Romeo Santos",
-                "genres": [
-                    "bachata",
-                    "latin hip hop",
-                    "latin pop",
-                    "urbano latino"
-                ],
-                "title": "Eres Mía",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:6I86RF3odBlcuZA9Vfjzeq",
-                "weight": 5.28964254738048
-            },
-            {
-                "artist": "Romeo Santos",
-                "genres": [
-                    "bachata",
-                    "latin hip hop",
-                    "latin pop",
-                    "urbano latino"
-                ],
-                "title": "Amor Enterrado",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:3cdTfwai1PtCGOd9DIBMNU",
-                "weight": 5.28964254738048
-            },
-            {
-                "artist": "Zacarias Ferreira",
-                "genres": [
-                    "bachata",
-                    "bachata dominicana"
-                ],
-                "title": "Es Tan Difícil",
-                "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
-                "uri": "spotify:track:0Ihu6hcj4hrWVz22W3G10P",
-                "weight": 2.5905292832732236
-            }
-        ]
+        {
+            "artist": "Nicky Jam",
+            "genres": [
+                "latin hip hop",
+                "reggaeton",
+                "trap latino",
+                "urbano latino"
+            ],
+            "title": "X (feat. Maluma & Ozuna) - Remix",
+            "image": "https://i.scdn.co/image/ab67616d0000b27343ec4e5bf12be32fc0beaac2",
+            "uri": "spotify:track:6JjPBQfI2Y8nIjnm65X6Pw",
+            "weight": 5.158460415146735
+        },
+        {
+            "artist": "Ozuna",
+            "genres": [
+                "puerto rican pop",
+                "reggaeton",
+                "trap latino",
+                "urbano latino"
+            ],
+            "title": "Bebé",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:0ulsRBiciReng91DhfVT9D",
+            "weight": 4.720726046469784
+        },
+        {
+            "artist": "Jay Chou",
+            "genres": [
+                "c-pop",
+                "mandopop",
+                "taiwan pop",
+                "zhongguo feng"
+            ],
+            "title": "蒲公英的約定",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:3JovAXFjc98TkksMfeyIMh",
+            "weight": 2.884160497896886
+        },
+        {
+            "artist": "Prince Royce",
+            "genres": [
+                "bachata",
+                "latin hip hop",
+                "latin pop",
+                "urbano latino"
+            ],
+            "title": "Cosas de la Peda (feat. Gabito Ballesteros)",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:1RzNRKntEk0KiQE4NFBKmc",
+            "weight": 5.28964254738048
+        },
+        {
+            "artist": "Romeo Santos",
+            "genres": [
+                "bachata",
+                "latin hip hop",
+                "latin pop",
+                "urbano latino"
+            ],
+            "title": "Mi Santa (feat. Tomatito)",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:4EbAftNM732UGLF8gmIIsX",
+            "weight": 5.28964254738048
+        },
+        {
+            "artist": "CNCO",
+            "genres": [
+                "boy band",
+                "latin pop",
+                "reggaeton",
+                "urbano latino"
+            ],
+            "title": "Reggaetón Lento (Bailemos)",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:3AEZUABDXNtecAOSC1qTfo",
+            "weight": 4.556474012983767
+        },
+        {
+            "artist": "Ozuna",
+            "genres": [
+                "puerto rican pop",
+                "reggaeton",
+                "trap latino",
+                "urbano latino"
+            ],
+            "title": "Egoísta",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:4X3CV9rXo3hQrkb0fzRAux",
+            "weight": 4.720726046469784
+        },
+        {
+            "artist": "Romeo Santos",
+            "genres": [
+                "bachata",
+                "latin hip hop",
+                "latin pop",
+                "urbano latino"
+            ],
+            "title": "Eres Mía",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:6I86RF3odBlcuZA9Vfjzeq",
+            "weight": 5.28964254738048
+        },
+        {
+            "artist": "Romeo Santos",
+            "genres": [
+                "bachata",
+                "latin hip hop",
+                "latin pop",
+                "urbano latino"
+            ],
+            "title": "Amor Enterrado",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:3cdTfwai1PtCGOd9DIBMNU",
+            "weight": 5.28964254738048
+        },
+        {
+            "artist": "Zacarias Ferreira",
+            "genres": [
+                "bachata",
+                "bachata dominicana"
+            ],
+            "title": "Es Tan Difícil",
+            "image": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1",
+            "uri": "spotify:track:0Ihu6hcj4hrWVz22W3G10P",
+            "weight": 2.5905292832732236
+        }
+    ]
     const [responses, setResponses] = useState([]);
     const [songCount, setSongCount] = useState(0);
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
@@ -253,7 +253,7 @@ function SessionPage() {
     return (
         <div>
             <img src={sessionpagebackground} alt="background wave" className="fixed inset-0 w-full h-full object-cover z-0"
-                style={{"zIndex": -1}}/>
+                 style={{"zIndex": -1}}/>
             {!submit && <div>
                 <Fade in={!submit} transition={{exit: {duration: .25}, enter: {duration: 1}}}>
                     <div className="mt-20 text-center text-xl sm:text-2xl font-mono mb-20 text-black">
