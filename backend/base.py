bf98c73dab70ce8fd511267d9acbb7d0ba1e876e
Sanjay Natesan <sn2967@columbia.edu>
2024-02-04 03:37:24 -0500

    Pre-session recommendations algorithms

--- backend/base.py
+++ backend/base.py
@@ -150,7 +150,7 @@ def add_user():
 
 
 @api.route('/recent', methods=['POST', 'GET'])
-@cross_origin(supports_credentials=True, origins='http://127.0.0.1:3000')
+@cross_origin(supports_credentials=True, origins=['http://127.0.0.1:3000', 'http://localhost:3000'])
 def recent():
     output = algorithms.rselect()
     response = jsonify(output)
@@ -296,6 +296,7 @@ def dummy():
 
 
 @api.route('/submit', methods=['POST'])
+@cross_origin(supports_credentials=True, origins=['http://127.0.0.1:3000', 'http://localhost:3000'])
 def submit_feelings():
     try:
         data = request.get_json()
@@ -304,10 +305,10 @@ def submit_feelings():
         not_feelings = data.get('not_feelings', [])
         username = data.get('username', None)
         song_uri = data.get('song_uri', None)
-        print(f'Received username: {username}')
-        print(f'Received feelings: {feelings}')
-        print(f'Received written feelings: {written_feelings}')
-        print(f'Not feelings: {not_feelings}')
+        # print(f'Received username: {username}')
+        # print(f'Received feelings: {feelings}')
+        # print(f'Received written feelings: {written_feelings}')
+        # print(f'Not feelings: {not_feelings}')
 
         conn = get_database_connection()
         curr = conn.cursor()
@@ -318,29 +319,29 @@ def submit_feelings():
         # Extract emotions from the result and concatenate into a string
         emotions_string = ', '.join(emotion[0] for emotion in emotions_result)
 
-        print(f'Emotions from user_songs: {emotions_string}')
+        # print(f'Emotions from user_songs: {emotions_string}')
 
         unique_emotions = ', '.join(set(re.split(r',\s|\s|,', emotions_string)))
-        print(f'Unique Emotions: {unique_emotions}')
+        # print(f'Unique Emotions: {unique_emotions}')
 
         predefined_emotions = "jaded, excited, faithful, happy, wistful, salacious"
 
         # Take the set union of unique emotions and predefined emotions
         combined_emotions = ', '.join(set(re.split(r',\s|\s|,', unique_emotions)) | set(re.split(r',\s|\s|,', predefined_emotions)))
-        print(f'Combined Emotions: {combined_emotions}')
+        # print(f'Combined Emotions: {combined_emotions}')
 
         list_emotions = [emotion.strip() for emotion in combined_emotions.split(",")]
-        print(list_emotions)
+        # print(list_emotions)
 
         written_analysis = []
-
+        
         if len(written_feelings) > 0:
             try:
                 written_analysis = ''.join(gemini.training_vertex(combined_emotions, written_feelings).splitlines()[-1:])
                 written_analysis = written_analysis.lower()
                 written_analysis = list(set(re.split(r',\s|\s|,', written_analysis)))
                 written_analysis = written_analysis[-3:]
-                print(f'Written analysis: {written_analysis}')
+                # print(f'Written analysis: {written_analysis}')
 
                 for emotion in written_analysis:
                     if emotion != "":
@@ -350,10 +351,12 @@ def submit_feelings():
                         if count == 0:
                             # If no such entry exists, insert a new row
                             curr.execute("INSERT INTO user_songs (username, emotion, reaction, song_uri) VALUES (%s, %s, %s, %s)",
-                                        (username, emotion, True, song_uri))
+                                         (username, emotion, True, song_uri))
                 conn.commit()
             except Exception as e:
                 written_analysis = []
+
+
         for emotion in feelings:
             if emotion != "":
                 curr.execute("SELECT COUNT(*) FROM user_songs WHERE username = %s AND emotion = %s AND song_uri = %s", (username, emotion, song_uri))
@@ -382,6 +385,76 @@ def submit_feelings():
     except Exception as e:
         print(f'Error processing feelings: {str(e)}')
         return {'error': 'Failed to process feelings'}, 500
+
+
+@api.route('/presessionResultProcessing', methods=['POST', 'GET'])
+@cross_origin(supports_credentials=True, origins=['http://127.0.0.1:3000', 'http://localhost:3000'])
+def presessionResultProcessing():
+    try:
+        # data = request.get_json()
+        response = "Feeling good"#data.get('inputValue', [])
+        inputType = 1 #data.get('inputType', 0)
+        username = '9tlgjm5tb8iivhwr525qopqu7' #data.get('username', None)
+        # print(f'Received username: {username}')
+        # print(f'Received response: {response}')
+        # print(f'Received input type: {inputType}')
+
+        conn = get_database_connection()
+        curr = conn.cursor()
+
+
+        # 0 is button, 1 is written
+        if inputType == 1:
+            curr.execute("SELECT emotion FROM user_songs WHERE username = %s", (username,))
+            emotions_result = curr.fetchall()
+
+            # Extract emotions from the result and concatenate into a string
+            emotions_string = ', '.join(emotion[0] for emotion in emotions_result)
+
+            # print(f'Emotions from user_songs: {emotions_string}')
+
+            unique_emotions = ', '.join(set(re.split(r',\s|\s|,', emotions_string)))
+            # print(f'Unique Emotions: {unique_emotions}')
+
+            # list_emotions = [emotion.strip() for emotion in unique_emotions.split(",")]
+            # print(list_emotions)
+            # process
+            response = gemini.suggestions_vertex(unique_emotions, response)
+
+
+        curr.execute("SELECT song_uri, reaction FROM user_songs WHERE username = %s AND emotion = %s", (username, response,))
+        uri_reaction = curr.fetchall()
+
+        # uri_reaction_list = [(uri, reaction) for uri, reaction in uri_reaction if uri != ""]
+
+        uri_reaction_list = []
+        for uri, reaction in uri_reaction:
+            if uri != "":
+                uri_reaction_list.append((uri, reaction))
+
+
+        curr.close()
+        conn.close()
+
+        returnval = algorithms.get_recs(uri_reaction_list)
+        print(returnval)
+
+        to_return = {
+            "album": returnval["album"]["name"],
+            "artist": returnval["artists"][0]["name"],
+            "title": returnval["name"],
+            "song_uri": returnval["uri"],
+            "image": returnval["album"]["images"][0]["url"],
+            "response": response
+        }
+
+        print(to_return)
+
+        return to_return
+
+    except Exception as e:
+        print(f'Error processing result: {str(e)}')
+        return {'error': 'Failed to process result'}, 500
 
 
 if __name__ == '__main__':
